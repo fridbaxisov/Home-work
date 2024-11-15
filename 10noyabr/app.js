6e756ecb30a263a3eee4110fcd6274ac66e94ce5
fridbaxisov <fridbaxisov437@gmail.com>
2024-11-15 19:06:42 +0400

    created homework

---
+++ 10noyabr/app.js
@@ -0,0 +1,106 @@
+// let a="oyrenirem"
+// let b="ders"
+
+
+// console.log(a+" "+b);
+
+// let a = "Ders";
+// let b = 1;
+// let c = a+b;
+// console.log(c.repeat(4));
+
+//  let yas=56
+//  if(yas>5 && yas<18){
+//      console.log("usaqdir");
+//  }
+//  else if(yas>18 && yas<24) {
+//     console.log("genc");
+// }
+// else if(yas>24 && yas<50){
+//     console.log("yaslisiz");
+// }else{
+//     console.log("ahilsiz");
+// }
+
+// switch (true) {
+//     case (yas>5 && yas<18):
+//     console.log("usaqdir");
+
+//     break;
+//     case(yas>18 && yas<24):
+//     console.log("genc");
+
+//     break;
+//     case(yas>24 && yas<50):
+//     console.log("yaslisiz");
+
+//     break;
+
+//     default:
+//         console.log("ahilsiz");
+//         break;
+
+// }
+
+// (yas>5 && yas<18)?console.log("usaqdir"):(yas>24 && yas<50)? console.log("genc"): console.log("ahilsiz");
+
+
+// tapsiriq 1
+// let a = 40;
+// let b = 15;
+// let c = 30;
+// let d = 20;
+
+// if(a%b==0 && c%d==0){
+// console.log(a+b+c+d);
+// }
+// else if (a%b==0 && c%d!=0){
+//     console.log(a+b+c-d);
+// }
+// else if(a%b!=0  && c%d==0){
+//     console.log(a-b+c+d);
+// }
+// else{
+//     console.log(a,b,c,d);
+// }
+
+//tapsiriq 6
+// let a = "men";
+// let b = "proqramlasdirma";
+// let c = "oyrenirem";
+
+// console.log(a+" "+b+" "+c);
+
+
+
+//tapsiriq 7 tam deyil
+// let a ="ders";
+// let b = "1";
+// console.log(a.repeat(3));
+
+// 2 ci nin 1 cisi
+// let a = 5.60;
+// console.log(Math.floor(a));
+
+// 2 cinin 3 cusu
+// let ad = "Farid";
+// let soyad = "Baxisov";
+// let yas = 17;
+
+// console.log(ad+" "+soyad+" "+yas)
+
+// function funk (Name,SurName,year,birthday){
+//     let age = year-birthday
+ 
+
+// console.log(`${Name} ${SurName} ${birthday} ilinde anadan olub ve $ {age} yasi var`); }
+
+// funk("Farid","Baxisov",2024,1988);
+
+
+
+
+
+
+
+
